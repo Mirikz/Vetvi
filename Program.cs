0b648cb4f8be5ab80c240fc3d467ea0d81cab2af
Mirikz <kirillmitrofanov68@gmail.com>
2022-01-26 09:29:50 +0500

    b func

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int in_b()
+{
+    Console.WriteLine("Enter b");
+    return int.Parse(Console.ReadLine());
+}
